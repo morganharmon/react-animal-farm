2147609eb1b1c6e7775ac1bc32aaa6a2cae368c4
Morgan Harmon <morganharmon@gmail.com>
2022-06-07 16:05:31 -0700

    Fix netlify error

--- src/animal/animal.js
+++ src/animal/animal.js
@@ -4,8 +4,8 @@ import './animal.css';
 export default function Animal({ name, type, says, top, left }) {
   return (
     <div className='animal' style={{ width: '200px', position: 'absolute', top, left }}>
-      <p>{ name }</p>
       <img src={`/animals/${type}.svg`}/>
+      <p>{ name }</p>
       <p>{ says }</p>
     </div>
   );
